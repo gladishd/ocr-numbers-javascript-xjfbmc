2ddc849774aa616de752f1f584d37d69b61aaaaa
gladishd <gladish.dean@gmail.com>
2022-11-06 00:39:29 -0400

    First commit of the test. Could we just read the template literal directly? Who knows. CodeSubmit - Real-world coding interviews… * VotingWorks. OCR Numbers - JavaScript Hiring Test. * How does the “Welcome back Dean! It looks like you already started an assignment with VotingWorks. Enter the same email to continue where you left off.” Dismiss * Work? Cookies? The UI is designed with the Dismiss button is just a bold string of text. * The Placeholders - First Name: e.g. Steve, Last Name e.g. Wozniak, E-Mail e.g. wozniak@apple.com; what a nice touch! It is equally amazing that we are able to do this from the perspective of someone like Steve Jobs who designed the Mathematica name and the borders around the cells. * We press “Play” and are taken to the View Assignment page. * Using my email gladish.dean@gmail.com for the sake of “consistency”, * Welcome to VotingWorks’s take-home assignment OCR Numbers - JavaScript for the Software Engineer role. We’re excited to have you here! * “We hope you find this experience challenging but enjoyable.” Good sentence there that’s why we came. * “I promise not to plagiarize or copy the work of others.” Thank you for writing this, what is it going to mean to someone like me who hasn’t even gotten started? * You will be using GIT to clone the assignment repository and submit your work. Make sure your git client is ready and configured. You’re writing about the “native” installation of Git, right? The one on iOS Terminal? * “As part of this assignment, you will write JavaScript code against test files that are included in the repository.” I see you’re running the tests with Jest. Just remember that a lot of us are so grateful to have a chance to work with Jest in-code!
    * “Within the repository, you will find detailed instructions on how to complete the task.”
    * “We encourage you to commit and push your changes regularly. This way, the hiring manager at VotingWorks can see your thought process through your commit history.” Suppose that helps us get started right? `git push origin master`. And the fact that this “starter code” is able to get us started is miraculous.
    
    * Given a 3 x 4 grid of pipes, underscores, and spaces…
    * determine which number is represented, or whether it is garbled.
    * Step One. To begin with, convert a simple binary font to a string containing 0 or 1.
    * The binary font uses pipes and underscores, four rows high and three columns wide.
    * Something “needs to be added” to this description… the binary font is a drawing of a number that tis, and yes it uses pipes and underscores.
    * Very interesting that you’re using bash as a means of accessing the `npm install` command.. didn’t think of that before.
    * Suppose that this website functions much like a timer. It would cool to see some secret messages on there like we have on the Wolfram Community.
    * That was the most interesting but brief project description leaving us searching for more.
    * The export const convert = () => { throw new Error(“Remove this statement and implement this function”); };
    * They tell you to remove it but never to comment it out.

--- ocr-numbers.js
+++ ocr-numbers.js
@@ -1,4 +1,43 @@
-
-export const convert = () => {
-  throw new Error("Remove this statement and implement this function");
+export const convert = (whatAreTheProps) => {
+  // throw new Error("Remove this statement and implement this function");
+  console.log(whatAreTheProps);
 };
+
+/** The binary font uses pipes and underscores,
+    * four rows high and three columns wide.
+    * what else does it do?
+    * It "looks like" a readable number.
+    *
+    * We're going to have to pass a variety of specs including the fact that the OCR recognizes numbers like zero, one, two, three, four, five, six, seven, eight, nine, ten, garble, 110101100, garble mixed in, 1234567890, and 123 456 789.
+    *
+    *
+    * */
+
+/**
+ * CodeSubmit - Real-world coding interviews…
+* VotingWorks. OCR Numbers - JavaScript Hiring Test.
+* How does the “Welcome back Dean! It looks like you already started an assignment with VotingWorks. Enter the same email to continue where you left off.” Dismiss
+* Work? Cookies? The UI is designed with the Dismiss button is just a bold string of text.
+* The Placeholders - First Name: e.g. Steve, Last Name e.g. Wozniak, E-Mail e.g. wozniak@apple.com; what a nice touch! It is equally amazing that we are able to do this from the perspective of someone like Steve Jobs who designed the Mathematica name and the borders around the cells.
+* We press “Play” and are taken to the View Assignment page.
+* Using my email gladish.dean@gmail.com for the sake of “consistency”,
+* Welcome to VotingWorks’s take-home assignment OCR Numbers - JavaScript for the Software Engineer role. We’re excited to have you here!
+* “We hope you find this experience challenging but enjoyable.” Good sentence there that’s why we came.
+* “I promise not to plagiarize or copy the work of others.” Thank you for writing this, what is it going to mean to someone like me who hasn’t even gotten started?
+* You will be using GIT to clone the assignment repository and submit your work. Make sure your git client is ready and configured. You’re writing about the “native” installation of Git, right? The one on iOS Terminal?
+* “As part of this assignment, you will write JavaScript code against test files that are included in the repository.” I see you’re running the tests with Jest. Just remember that a lot of us are so grateful to have a chance to work with Jest in-code!
+
+* “Within the repository, you will find detailed instructions on how to complete the task.”
+* “We encourage you to commit and push your changes regularly. This way, the hiring manager at VotingWorks can see your thought process through your commit history.” Suppose that helps us get started right? `git push origin master`. And the fact that this “starter code” is able to get us started is miraculous.
+
+* Given a 3 x 4 grid of pipes, underscores, and spaces…
+* determine which number is represented, or whether it is garbled.
+* Step One. To begin with, convert a simple binary font to a string containing 0 or 1.
+* The binary font uses pipes and underscores, four rows high and three columns wide.
+* Something “needs to be added” to this description… the binary font is a drawing of a number that tis, and yes it uses pipes and underscores.
+* Very interesting that you’re using bash as a means of accessing the `npm install` command.. didn’t think of that before.
+* Suppose that this website functions much like a timer. It would cool to see some secret messages on there like we have on the Wolfram Community.
+* That was the most interesting but brief project description leaving us searching for more.
+* The export const convert = () => { throw new Error(“Remove this statement and implement this function”); };
+* They tell you to remove it but never to comment it out.
+ */
