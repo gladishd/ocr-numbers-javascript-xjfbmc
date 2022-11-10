bfee78c66bb4a2378bb08c431c1314b9eb235422
gladishd <gladish.dean@gmail.com>
2022-11-09 22:20:59 -0500

    Condenses definitions of integers. Can we have a default return switch that doesn’t get invoked unless it’s gibberish? How do we identify blank lines of spaces? For those objects, at least those from the examples, we have 4, and 7. We can map -> console.log: rows.map(row => console.log(row));. Suppose that it’s natural for each letter to have a “height” of 4. But in “reality” this is the example case where integers are stacked on top of each other. Is this the less than less than case when we can do <<, or just write < rows.length - 4? We can still push things to a const array, that is “best defined” in a for-loop. We didn’t need all those switch statements at the beginning! Yes, 1 and 0 can make 10 when joined at the end, and we didn’t even know! Want to debug across all scenarios at once. It skips over the console logs unless it runs. On returning “?”, this handles the case in which the switch function on (e), within the .map function doesn’t reach the 0, 1, …, 9, and when we get past this, return “?”. The conversion that we need to do is convert and return 123 456 789. When there’s nothing there, what do we return? Don’t return inside of a .reduce. The console.log wrapper can fundamentally change the behavior of the enclosed function, theGridQuantum.reduce. To make a more realistic sequence of numbers, let’s make an exception for 10 and simply look at whether or not the width of the string `e` is greater than 3. Switch may or may not impact the return statement. * Removes first console log throw new Error("Remove this statement and implement this function"); * Take the next 4 rows, the height of the integer symbols. What is the length of a row? How can we iterate over the length of that row and then some? * This is our starting point. Let’s go 4 rows down. * Can we mix indexing in strings and rows? Now want to go across the columns 3 times. * We can start out looking at [i + k][j + m]… and then we can interchange with the index at that row! Instead of removing the last element from the array. * This is “awfully specific”. We “need” to iterate “down” that row. Then, i stays the same. Want to iterate over all rows. * First of all, the Grid Quantum in this case, what is it? We need to go to that level. It is a list of strings describing different numbers, therefore what is Grid Quantum when it’s not multiple things?   * We already caught that case of 10, where the input props match the symbol of 10.   * What is each element of Grid Quantum, consecutively?   * Can we return 10 when we get a case of ten? No! This case is handled at the very beginning. Remove it.   * Then we’re going to work together and split the number, in its expected form, into its constituents.   * Then, evaluate each constituent and convert the numeric form into a symbol. Yes, the length of e is greater than 3. But what is it? e is an integer. * Turn the .reduce statement into .map, so that we aren’t getting a string as the result. CodeSignal say don't write in the test specs.. https://exercism.org/tracks/javascript/exercises/ocr-numbers/solutions/kylejw2 Given a 3 x 4 grid of pipes as in README.md and on the above solution to the OCR number puzzle, we are able to see that each letter has a height of 4 and a width of 3. Instead of parsing each letter out individually, "what" if we could parse them out by column? Except then we'd need to parse out numbers like '100', which would require a "prohibitively expensive" table of values.

--- ocr-numbers.js
+++ ocr-numbers.js
@@ -1,132 +1,59 @@
 export const convert = (whatAreTheProps) => {
-  // throw new Error("Remove this statement and implement this function");
-  let split = whatAreTheProps.split('');
-  let zero = ' _ \n'
+  const zero = ' _ \n'
     + '| |\n'
     + '|_|\n'
-    + '   ';
-  let one = '   \n'
-    + '  |\n'
-    + '  |\n'
-    + '   ';
-  if (JSON.stringify(split) == JSON.stringify(zero)) return "0";
-  if (JSON.stringify(split) == JSON.stringify(one)) return "1";
-  if (whatAreTheProps == ' _ \n'
-    + ' _|\n'
-    + '|_ \n'
-    + '   ') return "2";
-  switch (whatAreTheProps) {
-    case ' _ \n'
-      + ' _|\n'
-      + ' _|\n'
-      + '   ': return "3"
-    case '   \n'
-      + '|_|\n'
+    + '   ',
+    one = '   \n'
       + '  |\n'
-      + '   ': return "4"
-    case ' _ \n'
-      + '|_ \n'
+      + '  |\n'
+      + '   ',
+    nine = ' _ \n'
+      + '|_|\n'
       + ' _|\n'
-      + '   ': return "5"
-    case ' _ \n'
+      + '   ',
+    eight = ' _ \n'
+      + '|_|\n'
+      + '|_|\n'
+      + '   ',
+    seven = ' _ \n'
+      + '  |\n'
+      + '  |\n'
+      + '   ',
+    six = ' _ \n'
       + '|_ \n'
       + '|_|\n'
-      + '   ': return "6"
-    case ' _ \n'
-      + '  |\n'
-      + '  |\n'
-      + '   ': return "7"
-    case ' _ \n'
-      + '|_|\n'
-      + '|_|\n'
-      + '   ': return "8"
-    case ' _ \n'
-      + '|_|\n'
+      + '   ',
+    five = ' _ \n'
+      + '|_ \n'
       + ' _|\n'
-      + '   ': return "9"
-    case '    _ \n'
-      + '  || |\n'
-      + '  ||_|\n'
-      + '      ': return "10"
-    case '   \n'
-      + '| |\n'
-      + '| |\n'
-      + '   ': return "?"
-  }
-  let ten = '    _ \n'
-    + '  || |\n'
-    + '  ||_|\n'
-    + '      ';
-  let nine = ' _ \n'
-    + '|_|\n'
-    + ' _|\n'
-    + '   ';
-  let eight = ' _ \n'
-    + '|_|\n'
-    + '|_|\n'
-    + '   ';
-  let seven = ' _ \n'
-    + '  |\n'
-    + '  |\n'
-    + '   ';
-  let six = ' _ \n'
-    + '|_ \n'
-    + '|_|\n'
-    + '   ';
-  let five = ' _ \n'
-    + '|_ \n'
-    + ' _|\n'
-    + '   ';
-  let four = '   \n'
-    + '|_|\n'
-    + '  |\n'
-    + '   ';
-  let three = ' _ \n'
-    + ' _|\n'
-    + ' _|\n'
-    + '   ';
-  let two = ' _ \n'
-    + ' _|\n'
-    + '|_ \n'
-    + '   ';
-  let rows = whatAreTheProps.split('\n');
-  let theGridQuantum = [];
-
+      + '   ',
+    four = '   \n'
+      + '|_|\n'
+      + '  |\n'
+      + '   ',
+    three = ' _ \n'
+      + ' _|\n'
+      + ' _|\n'
+      + '   ',
+    two = ' _ \n'
+      + ' _|\n'
+      + '|_ \n'
+      + '   '
+  const rows = whatAreTheProps.split('\n');
+  const theGridQuantum = [];
   for (let i = 0; i < rows.length; i += 4) {
-    // Take the next 4 rows, the height of the integer symbols
-    let currentColumn;
-    // What is the length of a row?
-    let length = rows[i].length;
-    /** How can we iterate over the length
-     * of that row and then s0ke?
-     */
+    const length = rows[i].length;
     for (let j = 0; j < length; j += 3) {
-      currentColumn = [];
-      /**
-       * This is our starting point. Let's go
-       * 4 rows down.
-       */
+      const currentColumn = [];
       for (let k = 0; k < 4; k++) {
-        /** Can we mix indexing on strings and rows?
-          * Now want to go across the columns 3 times */
-        for (let m = 0; m < 3; m++) {
-          // console.log("What is this is is ", rows[i + k][j + m]);
-          currentColumn.push(rows[i + k][j + m]);
-        }
-        if (k < 3) currentColumn.push('\n');
-        /** Interchange with the index at that row!
-          * Instead of removing the last element from the array. */
+        for (let m = 0; m < 3; m++) currentColumn.push(rows[i + k][j + m]);
+        k < 3 && currentColumn.push('\n');
       }
       theGridQuantum.push(currentColumn.join(''));
     }
-    /** This is "awfully specific".
-     * We "need" to iterate "down" that row. Then,
-     * i stays the same.
-     * Want to iterate over all rows.
-     */
+    i < rows.length - 4 && theGridQuantum.push(',');
   }
-  // console.log('NoW What IS THe Grid Quantum?', theGridQuantum, whatAreTheProps);
-  console.log("HLSDFLKDSLKFJ the reduced ", theGridQuantum.reduce((previous, e) => {
+  let grid = theGridQuantum.map((e) => {
     switch (e) {
       case zero: return "0";
       case one: return "1";
@@ -138,15 +65,9 @@ export const convert = (whatAreTheProps) => {
       case seven: return "7";
       case eight: return "8";
       case nine: return "9";
-      case ten: return "10"; // Yeah right!
+      case ",": return ",";
     }
-  }, ""));
-
-  // console.log('the new value is ', theGridQuantum[0], theGridQuantum[0] == zero, zero, theGridQuantum[0].split(''), zero.split(''));
+    return "?";
+  });
+  return grid.join('');
 };
-
-/** CodeSignal say don't write in the test specs..
-    https://exercism.org/tracks/javascript/exercises/ocr-numbers/solutions/kylejw2
-    Given a 3 x 4 grid of pipes as in README.md and on the above solution to the OCR number puzzle, we are able to see that each letter has a height of 4 and a width of 3. Instead of parsing each letter out individually, "what" if we could
-    parse them out by column? Except then we'd need to parse out numbers like '100', which would require a "prohibitively expensive" table of values.
- */
